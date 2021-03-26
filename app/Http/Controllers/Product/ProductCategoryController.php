98e5bf373ed839ce79c12200bcfdfdfa68eeb56d
diego <diegomotta18@gmail.com>
2021-03-26 11:34:16 -0300

    metodo destroy en el controllador de productos categorias

--- app/Http/Controllers/Product/ProductCategoryController.php
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Product;
 
+use App\Category;
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
 use App\Product;
@@ -28,9 +29,11 @@ class ProductCategoryController extends ApiController
      * @param  \App\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request, Product $product,Category $category)
     {
         //
+        $product->categories()->syncWithoutDetaching([$category->id]);
+        return $this->showAll($product->categories);
     }
 
     /**
@@ -39,8 +42,14 @@ class ProductCategoryController extends ApiController
      * @param  \App\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product)
+    public function destroy(Product $product,Category $category)
     {
         //
+        if(!$product->categories->find($category->id)){
+                return $this->errorResponse("La categoría especificada no es una categoría de este producto",404);
+        }
+
+        $product->categories()->detach([$category->id]);
+        return $this->showAll($product->categories);
     }
 }
