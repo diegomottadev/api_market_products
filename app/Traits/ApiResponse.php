36b825266f3d5f743b716fa576d385867b2feda7
diego <diegomotta18@gmail.com>
2021-03-30 11:06:22 -0300

    ADD: use fractal y transformer en los modelos

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -14,13 +14,28 @@ trait ApiResponse{
     }
 
     protected function showAll(Collection $colletion,$code= 200){
-        return $this->successResponse(['data'=>$colletion],$code);
+
+        if($colletion->isEmpty()){
+            return  $this->successResponse(['data'=>$colletion],$code);
+        }
+        $transformer = $colletion->first()->transformer;
+
+        $colletion = $this->transformData($colletion,$transformer);
+        return $this->successResponse($colletion,$code);
     }
     protected function showOne(Model $instance,$code= 200){
-        return $this->successResponse(['data'=>$instance],$code);
+        $transformer = $instance->transformer;
+        $instance = $this->transformData($instance,$transformer);
+
+        return $this->successResponse($instance,$code);
     }
 
     protected function errorResponse($message,$code){
         return response()->json([   'error'=> $message,'code'=>$code],$code);
+    }
+
+    protected function transformData($data,$tranformer){
+        $transformation = fractal($data,new $tranformer);
+        return $transformation->toArray();
     }
 }
---
+++ app/Transformers/TransactionTransformer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Transaction;
+use League\Fractal\TransformerAbstract;
+
+class TransactionTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Transaction $transaction)
+    {
+        return [
+            //
+            'identificador' => (int)$transaction->id,
+            'cantidad' => (int)$transaction->quantity,
+            'comprador' => (int)$transaction->buyer_id,
+            'vendedor' => (int)$transaction->seller_id,
+            'cantidad' => (int)$transaction->quantity,
+            'fechaCreacion' => (string)$transaction->created_at,
+            'fechaActualizacion' =>(string) $transaction->updated_at,
+            'fechaEliminacion' => isset($transaction->deleted_at) ? (string) $transaction->deleted_at : null
+        ];
+    }
+}
